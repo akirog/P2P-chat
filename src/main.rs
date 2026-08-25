74c6d4e07bfe3208a5e75d3cdfbae41acfe238b1
aki <aki.rogeberg@gmail.com>
2026-08-25 20:11:06 +0200

    finished connection code, testing pending

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 use local_ip_address::local_ip;
-use tokio::net::{TcpStream, UdpSocket};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream, UdpSocket};
 use tokio::sync::Mutex;
+
 
 struct P2pNode {
     username: String,
@@ -24,25 +26,20 @@ impl P2pNode {
 
         broadcast_socket.set_broadcast(true)?;
 
-        println!("broadcasting presence from {}, on port 25", self.address);
+        println!("broadcasting presence from {}, on port 8001", self.address);
 
         loop {
             broadcast_socket.send_to(
                 self.username.as_bytes(),
-                "255.255.255.255:25"
+                "255.255.255.255:8001"
             ).await?;
-
-            println!("broadcasted presence");
 
             tokio::time::sleep(std::time::Duration::from_secs(5)).await;
         }
-
-
-        Ok(())
     }
 
     async fn listen_for_peers(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let listen_socket = UdpSocket::bind("0.0.0.0:25").await?;
+        let listen_socket = UdpSocket::bind("0.0.0.0:8001").await?;
 
         listen_socket.set_broadcast(false)?;
 
@@ -56,21 +53,44 @@ impl P2pNode {
             let name = String::from_utf8_lossy(&buffer[..len]);
             if name == self.username { continue; }
 
-            println!("received message from {}", &name);
+            println!("received broadcast from {}", &name);
 
             let mut address = sender.ip().to_string();
-            println!("message from ip: {}", address);
+            println!("broadcast from ip: {}", address);
 
-            address = format!("{}:8001", address);
-            let connection = TcpStream::connect(&address).await?;
-
+            address = format!("{}:8002", address);
             let mut peers = self.peers.lock().await;
+            if peers.contains_key(name.as_ref()) {
+                continue;
+            }
+
+            let mut connection = TcpStream::connect(&address).await?;
+            connection.write_all(format!("{}\r\n", self.username).as_bytes()).await?;
             peers.insert(name.to_string(), connection);
 
         }
+    }
 
 
-        Ok(())
+    async fn accept_connection_requests(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind("0.0.0.0:8002").await?;
+        println!("listening for tcp connections");
+
+        loop {
+            let (mut stream, addr) = listener.accept().await?;
+
+            let mut buffer = [0; 1024];
+            let length = stream.read(&mut buffer).await?;
+            if length < 1 { continue; }
+
+            let name = String::from_utf8_lossy(&buffer[..length]);
+            println!("{addr} connected as {name}");
+
+            let mut peers = self.peers.lock().await;
+            if peers.contains_key(name.as_ref()) { continue; }
+
+            peers.insert(name.to_string(), stream);
+        }
     }
 }
 
@@ -79,7 +99,36 @@ impl P2pNode {
 #[tokio::main]
 async fn main() {
 
-    let mut node = P2pNode::new(String::from("bob"), local_ip().unwrap().to_string());
+    let node = P2pNode::new(
+        String::from("bob"),
+        local_ip().unwrap().to_string()
+    );
 
-    println!("Hello, world!");
+    let node = Arc::new(node);
+
+    let connection_node = node.clone();
+    let listening_task = tokio::spawn(async move {
+       if let Err(e) = connection_node.accept_connection_requests().await {
+           println!("Listener: {e}");
+       }
+    });
+
+
+    let broadcast_listener_node = node.clone();
+    let broadcast_listener_task = tokio::spawn(async move {
+        if let Err(e) = broadcast_listener_node.listen_for_peers().await {
+            println!("Broadcast listener error: {}", e);
+        }
+    });
+
+
+    let broadcast_node = node.clone();
+    let broadcast_task = tokio::spawn(async move {
+        if let Err(e) = broadcast_node.broadcast_presence().await {
+            println!("Broadcasting error: {}", e);
+        }
+    });
+
+
+    tokio::try_join!(listening_task, broadcast_listener_task, broadcast_task).unwrap();
 }
